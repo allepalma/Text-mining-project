c7a7a8da31bc0b6d2c86045038b36acf6e969887
Alessandro Palma <alessandro.palma1998@hotmail.it>
2021-12-07 08:42:25 +0100

    Add bert_data_creation.py script

---
+++ bert_data_creation.py
@@ -0,0 +1,125 @@
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler
+from transformers import AutoTokenizer
+from sklearn.model_selection import train_test_split
+
+'''
+Read the data from a pre-processed CADEC dataset and process them into a format compatible with BERT
+'''
+
+class DataProcessor():
+    '''
+    Loads the data from a pre-processed CADEC named-entity dataset and creates a BERT dataset
+    '''
+    def __init__(self, filename, model, seed, batch_size = 32, max_length = 512):
+        # Set the device
+        if torch.cuda.is_available():
+            self.device = torch.device('cuda')
+        else:
+            self.device = torch.device('cpu')
+
+        self.max_length = max_length
+        self.filename = filename
+        self.seed = seed  # For test and train split
+        self.model = model
+        self.batch_size = batch_size
+
+        print('Parsing the data file...')
+        self.tokens, self.labels = self.sentence_parser()  # Obtain the sentences
+        self.label2id = self.get_label_encoding_dict()  # Initialize mapping of labels to ids
+        self.tokens_train, self.tokens_test, self.labels_train, self.labels_test = self.train_test_split()
+
+        # Initialize tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model)
+        # Tokenize for BERT
+        # Training set
+        self.tokenized_input_train = self.tokenizer(self.tokens_train, truncation=True, is_split_into_words=True,
+                                                    add_special_tokens=True, padding = 'max_length', max_length = self.max_length)
+        self.train_tags = self.get_bert_labels(self.tokenized_input_train, self.labels_train)
+
+        # Test set
+        self.tokenized_input_test = self.tokenizer(self.tokens_test, truncation=True, is_split_into_words=True,
+                                                    add_special_tokens=True, padding = 'max_length', max_length = self.max_length)
+        self.test_tags = self.get_bert_labels(self.tokenized_input_test, self.labels_test)
+
+        # Prepare the data so it is compatible with torch
+        self.y_train = torch.tensor(self.train_tags).to(self.device)
+        self.y_test = torch.tensor(self.test_tags).to(self.device)
+
+        self.train_dataloader = self.create_data_loaders(self.tokenized_input_train, self.y_train)
+        self.test_dataloader = self.create_data_loaders(self.tokenized_input_test, self.y_test)
+
+    def sentence_parser(self):
+        '''
+        Read the content of filename and parses it into labels and tokens
+        :return: tokens and labels: two lists containing the tokens and the labels in the dataset
+        '''
+        with open(self.filename, 'r') as f:
+            data_raw = f.read()
+        sentences = [sent.split('\n') for sent in data_raw.split('\n\n')[:-1]]
+        tokens = [[pair.split('\t')[0] for pair in sent] for sent in sentences]
+        labels = [[pair.split('\t')[1] for pair in sent] for sent in sentences]
+        return tokens, labels
+
+    def train_test_split(self):
+        '''
+        Splits the dataset into training and test observations
+        :return:
+        '''
+        X_train, X_test, y_train, y_test = train_test_split(self.tokens, self.labels, test_size = 0.20,
+                                                            random_state = self.seed)
+        return X_train, X_test, y_train, y_test
+
+    def get_label_encoding_dict(self):
+        '''
+        Given the training data, associate each distinct label to an id
+        :return: lab2id: a dictionary mapping unique labels to ids
+        '''
+        labels = []  # list of unique labels
+        for sent in self.labels:
+            for label in sent:
+                if label not in labels and label != 'O':
+                    labels.append(label)
+        # Sort labels by the first letter after B- and I- in the BIO tag
+        labels = ['O'] + sorted(labels, key=lambda x: x[2:])
+        lab2id = {lab: id for lab, id in zip(labels, range(len(labels)))}
+        return lab2id
+
+    def get_bert_labels(self, tokenized_words, labels):
+        '''
+        Align labels with the pre-processed token sequences
+        '''
+        labels_bert = []
+        for i, label in enumerate(labels):  # Loop over token sentences
+            # Map each tokenized word to its ID in the original sentence
+            word_ids = tokenized_words.word_ids(batch_index=i)
+            # Contains the label ids for a sentence
+            label_ids = []
+            for word_idx in word_ids:
+                # Special characters ([CLS], [SEP], [PAD]) set to -100
+                if word_idx is None:
+                    label_ids.append(-100)
+                # If a word is broken by wordpiece, just add as many labels as word chunk
+                else:
+                    label_ids.append(self.label2id[label[word_idx]])
+            labels_bert.append(label_ids)
+        return labels_bert
+
+    def create_data_loaders(self, bert_ds, labels):
+        '''
+        Create a dataset compatoble with torch
+        :param bert_ds: A tokenized object containing both input_ids and mask ids
+        :param labels: The label sequence asociated to the tokens
+        :return: A torch DataLoader object
+        '''
+        # Create the DataLoader for our training set
+        # So now only use the inputs, not the original data anymore
+        data = TensorDataset(torch.tensor(bert_ds['input_ids']), torch.tensor(bert_ds['attention_mask']), labels)
+        sampler = RandomSampler(data)
+        # For each data loader we need the data, a sampler and a loader
+        data_loader = DataLoader(dataset = data, sampler = sampler, batch_size = self.batch_size)
+        return data_loader
+
+
+data_processor = DataProcessor(filename='testwrite.txt', model = 'bert-base-uncased', seed=13, max_length = 512)
+
---
+++ dataset_creation.py
@@ -0,0 +1,356 @@
+import os
+import re
+import numpy as np
+from tqdm import tqdm
+
+class DataProcessor():
+    def __init__(self, doublecheck_labels=False):
+        self.doublecheck_labels = doublecheck_labels
+
+    def create_dataset(self, dir='cadec', writefile='dataset', discard=["DICLOFENAC-SODIUM.7.txt"]):
+        '''
+        Create dataset and write to file
+        '''
+        self.discarded_files = [file for file in discard]
+        data = self.get_data(dir)
+        self.write_data(writefile, data)
+
+    def read_file(self, file):
+        '''
+        Read file
+        '''
+        with open(file, 'r') as f:
+            data = f.read()
+        return data
+
+    def extract_labelinfo(self, label_text):
+        '''
+        Create list of label information
+        Extract label and start and end positions of the characters,
+        only for lines starting with 'T' (ignore 'AnnotatorNotes')
+        '''
+        label_text = [line.split('\t') for line in label_text.split('\n')[:-1]]
+        label_data = []
+        words_to_label = []
+        for info in label_text:
+            if info[0][0] == 'T':
+                # Extract all start and end char positions, separated by ' ' and ';'
+                label_data.append(re.split(' |;', info[1]))
+                # Save words as denoted in labelfile for doublecheck
+                words_to_label.append(re.split(' ', info[2]))
+        return label_data, words_to_label
+
+    def check_email(self, word):
+        '''
+        Check whether both @ and . occur in word
+        '''
+        if '@' in word and '.' in word:
+            return True
+        return False
+
+    def check_multiple_dots(self, word, char):
+        '''
+        Check whether '.' occurs (at least) twice
+        To catch websites and abbreviations, e.g. i.e.
+        '''
+        if word.count(char) > 1:
+            return True
+        return False
+
+    # TODO: "(this)." will for now turn into ['(', 'this', ').']
+            # Might want to separate ').'
+            # It is possible to treat (), [], " ", ' ', < >, {} combinations different from rest
+    def separate_nonalnumgroup(self, word, pos, end_pos):
+        '''
+        Separate group of neighbouring non-alphanumeric characters
+        into subwords
+        '''
+        skip_next_word = False
+        # Only non-alnums
+        if len(word) == end_pos-pos:
+            subwords = [word]
+        # Alnumgroup at beginning
+        elif pos == 0:
+            subwords = [word[pos:end_pos], word[end_pos:]]
+        # Alnumgroup at end
+        elif end_pos == len(word):
+            subwords = [word[:pos], word[pos:]]
+            skip_next_word = True
+        # Alnumgroup between alnums
+        else:
+            subwords = [word[:pos], word[pos:end_pos], word[end_pos:]]
+            skip_next_word = True
+        return subwords, skip_next_word
+
+    def separate_first_char(self, word):
+        '''
+        Separate first character from rest of string
+        '''
+        subword1 = word[0]
+        subword2 = word[1:]
+        return [subword1, subword2]
+
+    def separate_last_char(self, word):
+        '''
+        Separate last character from rest of string
+        '''
+        subword1 = word[:-1]
+        subword2 = word[-1]
+        return [subword1, subword2]
+
+    def separate_apostrophe_char(self, word, pos):
+        '''
+        Handle apostrophes specifically
+        n't: haven't -> have n't. But can't wil become: ca, n't
+        've: you've -> you 've
+        I'm: I'm -> I 'm
+        's: he's -> he 's
+        '''
+        skip_next_word = False
+        subwords = []
+        if not word[pos+1].isalnum():
+            subwords = [word[:pos], word[pos], word[pos+1:]]
+            return subwords, skip_next_word
+        # Check "n't" ( ' was already checked to not be last char)
+        elif word[pos-1].lower() == 'n' and word[pos+1].lower() == 't':
+            subwords = [word[:pos-1], word[pos-1:pos+2]]
+            if len(word) > pos+2:
+                subwords.append(word[pos+2:])
+            skip_next_word = True    
+            return subwords, skip_next_word
+
+        elif word[pos+1].lower() == 'm' or word[pos+1].lower() == 's':
+            subwords = [word[:pos], word[pos:pos+2]]
+            if len(word) > pos+2:
+                subwords.append(word[pos+2:])
+            skip_next_word = True
+            return subwords, skip_next_word
+
+        elif len(word) > pos+2: 
+            if word[pos+1].lower() == 'v' and word[pos+2].lower() == 'e':
+                subwords = [word[:pos], word[pos:pos+3]]
+                if len(word) > pos+3:
+                    subwords.append(word[pos+2:])
+                skip_next_word = True
+                return subwords, skip_next_word
+        return [word], skip_next_word
+                
+    def separate_word(self, word, pos):
+        '''
+        Separate word into three subwords,
+        i.e. char(s) before pos, char at pos, char(s) after pos
+        '''
+        subwords = [word[:pos], word[pos], word[pos+1:]]
+        return subwords
+
+    def create_subwords(self, word):
+        '''
+        Check if word contains a non-alphanumeric character,
+        and then whether it needs to be separated into subwords
+        '''
+        non_alnum = re.search("[^0-9a-zA-Z]+", word)
+        pos = non_alnum.start()
+        end_pos = non_alnum.end()
+        char = non_alnum.group(0)
+        skip_next_word = False
+        # Neighbouring non-alnums
+        if end_pos-pos > 1:
+            subwords, skip = self.separate_nonalnumgroup(word, pos, end_pos)
+            skip_next_word = skip
+        # First char to be separated
+        elif pos == 0:
+            subwords = self.separate_first_char(word)
+        # Last char to be separated
+        elif pos == len(word)-1:
+            subwords = self.separate_last_char(word)
+        # Apostrophe (not at start/end): special case
+        elif char =='\'':
+            subwords, skip = self.separate_apostrophe_char(word, pos)
+            skip_next_word = skip
+        # Check if word is email address
+        elif self.check_email(word):
+            subwords = [word]
+        # Check if word contains multiple dots
+        elif char == '.' and self.check_multiple_dots(word, char):
+            subwords = [word]
+        # In all other cases: separate non-alnum from the rest
+        else:
+            subwords = self.separate_word(word, pos)
+            # Skip next always true, because always single non-alnum within word
+            skip_next_word = True
+        return subwords, skip_next_word
+
+    def get_words_and_positions(self, text_file):
+        '''
+        Read text from textfile,
+        replace enters by spaces and split on spaces.
+        Iterate over words to check for non-alhpanumeric characters.
+        If this is the case, (potentially) separate the words into subwords,
+        insert the subwords into [text],
+        correctly save the position of the first character of each word
+        into [char_positions]
+        '''
+        text = self.read_file(text_file)
+        text = text.replace('\n', ' ')
+        text = text.split(' ')[:-1] # Last character will be '', discarced here
+        char_positions = []
+        i, position = 0, 0
+        while i<len(text):
+            word = text[i]
+            if not word.isalnum() and len(word) > 1:
+                subwords, skip_next = self.create_subwords(word)
+                # Only need to update when word was separated into subwords
+                if len(subwords) > 1:
+                    j = i
+                    del text[i]
+                    for subword in subwords:
+                        text.insert(j, subword)
+                        j += 1
+                    char_positions.append(position)
+                    position += len(subwords[0])
+                    if skip_next:
+                        char_positions.append(position)
+                        position += len(subwords[1])
+                        # Account for space
+                        if len(subwords) == 2:
+                            position += 1
+                        i += 1
+                # Current char position: + wordlength + space
+                else:
+                    char_positions.append(position)
+                    position += (len(word) + 1)
+            else:
+                char_positions.append(position)
+                position += (len(word) + 1)
+            i += 1
+        return text, char_positions
+
+    def double_check(self, i, text, word_index, words_to_label, to_label_index, file):
+        '''
+        Double check if words in text with special label correspond
+        to words in the labelfile (saved in [words_to_label]).
+        Create subwords where necessary for words in labelfile and 
+        check if they are the same words as in text
+            (this is not exactly identical to how subwords are created for [text],
+            therefore print mismatches to also check manually)
+        '''
+        word = text[word_index]
+        check_word = words_to_label[i][to_label_index]
+        if word != check_word:
+            sub_check_words, skip_next = self.create_subwords(check_word)
+            if len(sub_check_words) > 2 and skip_next == False:
+                pass # TODO
+            del words_to_label[i][to_label_index]
+            new_index = to_label_index
+            for new_word in sub_check_words:
+                try:
+                    words_to_label[i].insert(new_index, new_word)
+                except:
+                    words_to_label[i].append(new_word)
+                new_index += 1
+        if word != words_to_label[i][to_label_index]:
+            print(f'Error: word to label in text doesn\'t correspond to word in labelfile:')
+            print(f'Text: {text[word_index]}\tlabelfile: {words_to_label[i][to_label_index]}')
+            print(f'Filename: {file}\n')
+        return words_to_label
+
+    def get_labels(self, label_file, text, char_positions):
+        '''
+        For all words to be labeled as denoted in label_file:
+            find their index in [text] on basis of their starting character
+            (which is stored in [label_data]),
+            store the label in [labels] at the same index
+        Potentially do the double check
+        '''
+        label_text = self.read_file(label_file)
+        label_data, words_to_label = self.extract_labelinfo(label_text)
+        labels = ['O']*len(char_positions)
+        # For all lines with labeldata for word(s)
+        for i in range(len(label_data)):
+            label = label_data[i][0]
+            to_label_index = 0
+            # For all start positions belonging to the label
+            for j in range(1, len(label_data[i])-1, 2):
+                start_pos = int(label_data[i][j])
+                end_pos = int(label_data[i][j+1])
+                # Get index of first word to be labeled
+                word_index = char_positions.index(start_pos)
+                if j == 1:
+                    labels[word_index] = f'B-{label}'
+                    if self.doublecheck_labels:
+                        words_to_label = self.double_check(i, text, word_index, words_to_label, to_label_index, label_file)
+                    to_label_index += 1
+                else:
+                    labels[word_index] = f'I-{label}'
+                    if self.doublecheck_labels:
+                        words_to_label = self.double_check(i, text, word_index, words_to_label, to_label_index, label_file)
+                    to_label_index += 1
+                # TODO: Maybe need to check whether i+1 is in range...! What to do if not?
+                current_pos = start_pos + char_positions[word_index+1] - char_positions[word_index]
+                # Iterate over all words that have this label
+                while current_pos < end_pos:
+                    word_index = char_positions.index(current_pos)
+                    labels[word_index] = f'I-{label}'
+                    if self.doublecheck_labels:
+                        words_to_label = self.double_check(i, text, word_index, words_to_label, to_label_index, label_file)
+                    to_label_index += 1
+                    current_pos += (char_positions[word_index+1] - char_positions[word_index])
+        return labels
+
+    def get_data(self, directory):
+        '''
+        Extract text and corresponding labels from all files
+        in specified directory
+        '''
+        text_dir = os.path.join(directory, 'text')
+        label_dir = os.path.join(directory, 'original')
+        if not os.path.isdir(text_dir):
+            print(f'\nError: directory to textfiles does not exist.')
+            return [[],[]]
+        if not os.path.isdir(label_dir):
+            print(f'\nError: directory to labelfiles does not exist.')
+            return [[],[]]
+
+        text, labels = [], []
+        print(f'\nReading messages and constructing word-label pairs...')
+        for filename in tqdm(os.listdir(text_dir)):
+            if filename not in self.discarded_files:
+                text_file = os.path.join(text_dir, filename)
+                label_file = os.path.join(label_dir, f'{filename[:-3]}ann')
+                try:
+                    message_text, message_char_positions = self.get_words_and_positions(text_file)
+                except:
+                    print(f'Error while parsing test from file: {text_file}')
+                    return
+                try:
+                    message_labels = self.get_labels(label_file, message_text, message_char_positions)
+                except:
+                    print(f'Error while parsing labels from file: {label_file}')
+                    return
+                text.append(message_text)
+                labels.append(message_labels)
+        return [text, labels]
+
+    def write_data(self, filename, data):
+        '''
+        Write words and corresponding labels to txt file
+        '''
+        n_messages = len(data[0])
+        print(f'\nWriting words and corresponding labels of {n_messages} messages to {filename}...')
+        print(f'\t(Discarded files: {self.discarded_files})')
+        with open(filename, 'w') as f:
+            for i in tqdm(range(n_messages)):
+                message_txt = data[0][i]
+                message_labels = data[1][i]
+                for i, (word, label) in enumerate(zip(message_txt, message_labels)):
+                    if i==len(message_txt)-1:
+                        f.write(f'{word}\t{label}\n\n')
+                    else:
+                        f.write(f'{word}\t{label}\n')
+
+
+if __name__ == '__main__':
+    data_dir = 'cadec'
+    writefile = 'testwrite.txt'
+    p = DataProcessor()
+    p.create_dataset(data_dir, writefile)
