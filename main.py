b08553a07ed94ee5d587d0118a2a04be7690a160
Laurens Engwegen <laurensengwegen@gmail.com>
2021-12-18 20:40:25 +0100

    Added training and testing of models

--- main.py
+++ main.py
@@ -1,33 +1,56 @@
 import os
 from dataset_creation import DatasetCreator
 from bert_data_creation import DataProcessor
+from traintest import TrainTest
 from models.models import *
 from models.config import CustomBertConfig
 from torch import nn
 
 
-seed = 13
-dataset_file = 'dataset.txt'
-max_length = 512
+def baseline(seed, max_length, dataset_file):
+    # Class containing the data
+    data_loader = DataProcessor(filename=dataset_file, model='bert-base-uncased', seed=seed, max_length=max_length,
+                                batch_size=4)
+    # Configure parameters
+    config = CustomBertConfig(clf_type='baseline', num_labels=9, embedding_size=256, vocab_size=30522, hidden_size=128)
+    # Initialize model
+    bert_model = Baseline(config)
+    # Train and test the model
+    TrainTest(bert_model, 'Baseline', data_loader, learning_rate=1e-3)
 
-# Create dataset if it has not been created yet
-if not os.path.isfile(dataset_file):
-    data_dir = 'cadec'
-    c = DatasetCreator()
-    c.create_dataset(data_dir, dataset_file)
+def bertmodels(seed, max_length, dataset_file):
+    transformers = ['bert-base-uncased', 'dmis-lab/biobert-v1.1']
+    heads = {
+        'Linear': BertLinear,
+        'CRF': BertCRF,
+        'LSTM': BertLSTM
+    }
+    for transformer in transformers:
+        for model in heads.keys():
+            # Class containing the data
+            data_loader = DataProcessor(filename=dataset_file, model=transformer, seed=seed, max_length=max_length,
+                                        batch_size=4)
+            # Configure parameters
+            config = CustomBertConfig(model=transformer, clf_type='linear', num_labels=9,
+                                    dropout=0.1, hidden_size=768, num_clf_hidden_layers=0, num_neurons=(),
+                                    activation=nn.ReLU)
+            # Initialize model
+            bert_model = heads[model](config)
+            # Train and test the model
+            model_name = f'{transformer} + {model}'
+            TrainTest(bert_model, model_name, data_loader)
 
 
-###### Example on how to use the BERT model with linear head for prediction ######
-# Class containing the data
-data_loader = DataProcessor(filename=dataset_file, model='bert-base-uncased', seed=seed, max_length=max_length,
-                            batch_size=8)
-print(data_loader.label2id)
-# Configure parameters
-config = CustomBertConfig(clf_type='baseline', num_labels=9, embedding_size=256, vocab_size=30522, hidden_size=128)
+if __name__ == '__main__':
+    seed = 13
+    dataset_file = 'dataset.txt'
+    max_length = 512
 
-# Initialize model
-model = Baseline(config)
-batch = [b for b in data_loader.train_dataloader][0]
-
-# Predict
-res = model(input_ids=batch[0], attention_mask = batch[1], labels = batch[2])
+    # Create dataset if it has not been created yet
+    if not os.path.isfile(dataset_file):
+        data_dir = 'cadec'
+        c = DatasetCreator()
+        c.create_dataset(data_dir, dataset_file)
+    
+    baseline(seed, max_length, dataset_file)
+    # bertmodels(seed, max_length, dataset_file)
---
+++ traintest.py
@@ -0,0 +1,199 @@
+import logging
+import torch
+from sklearn.metrics import f1_score, precision_score, recall_score, classification_report
+from tqdm import tqdm
+
+class TrainTest():
+    def __init__(self,
+                 model,
+                 model_name,
+                 dataloader,
+                 optimizer=torch.optim.Adam,
+                 learning_rate=2e-5,
+                 early_stopping=True,
+                 max_epochs=30,
+                 logging=True
+                 ):
+        # Initialize logger to store results
+        self.logging = logging
+        if self.logging:
+            self.logger = self.initialize_logger('training_logger.log')
+
+        # Define model and dataloader
+        self.model = model
+        self.model_name = model_name
+        self.dataloader = dataloader
+
+        # Check for GPU
+        if torch.cuda.is_available():    
+            self.device = torch.device('cuda')
+        else:
+            self.device = torch.device('cpu')
+        self.model.to(self.device)
+
+        # Initialize training parameters
+        self.optimizer = optimizer(self.model.parameters(), lr=learning_rate)
+        self.early_stopping = early_stopping
+        self.max_epochs = max_epochs
+
+        # Train and test
+        print(f'\nStart training for {self.model_name}...\n')
+        if self.logging:
+            self.logger.info(f'\nStart training for {self.model_name}...\n')
+        self.train()
+        print(f'\nTesting trained model {self.model_name}...\n')
+        if self.logging:
+            self.logger.info(f'\nTesting trained model {self.model_name}...\n')
+        self.evaluate('Test')
+
+    def initialize_logger(self, log_file):
+        '''
+        Initialize loggerfile
+        '''
+        log_format = '%(asctime)s : %(message)s'
+        logging.basicConfig(format=log_format, filename=log_file, level=logging.DEBUG)
+        return logging.getLogger()
+
+    def log_metrics(self, avg_loss, f1, precision, recall, data):
+        '''
+        Log metrics during training/validating/testing
+        '''
+        self.logger.info(f'{data} avg. loss:\t{avg_loss}')
+        self.logger.info(f'{data} F1-score:\t{f1}')
+        self.logger.info(f'{data} precision:\t{precision}')
+        self.logger.info(f'{data} recall:\t{recall}')
+
+    def get_added_tokens(self):
+        '''
+        Return IDs of padding/separator/cls tokens
+        '''
+        padding = self.dataloader.tokenizer.vocab['[PAD]']
+        separator = self.dataloader.tokenizer.vocab['[SEP]']
+        classifier = self.dataloader.tokenizer.vocab['[CLS]']
+        return padding, separator, classifier
+
+    def get_metrics(self, y_true, y_pred):
+        '''
+        Return F1-score, precision and recall
+        '''
+        f1 = f1_score(y_true, y_pred, average="weighted", zero_division=0)
+        precision = precision_score(y_true, y_pred, average="weighted", zero_division=0)
+        recall = recall_score(y_true, y_pred, average="weighted", zero_division=0)
+        return f1, precision, recall
+
+    def train_epoch(self):
+        '''
+        Train model for a single epoch and return loss averaged over batches
+        '''
+        total_loss = 0
+        # Train mode
+        self.model.train()
+        for batch in tqdm(self.dataloader.train_dataloader):
+            # Get data
+            input_ids = batch[0].to(self.device)
+            attention_mask = batch[1].to(self.device)
+            labels = batch[2].to(self.device)
+            # Set gradient to zero
+            self.model.zero_grad()
+            # Get model output
+            output = self.model(input_ids = input_ids, 
+                              attention_mask = attention_mask,
+                              labels = labels)
+            loss = output['loss']
+            total_loss += loss.item()
+            # Compute gradient
+            loss.backward()
+            # Update weights
+            self.optimizer.step()
+        return total_loss/len(self.dataloader.train_dataloader)
+
+    def train(self):
+        '''
+        Train model and stop on basis of early stopping criterion
+        '''
+        no_improve_cnt = 0
+        prev_f1 = 0
+        for i in range(self.max_epochs):
+            print(f'\nEpoch {i+1}')
+            # Train and get avg. training loss over batches
+            avg_loss_tr = self.train_epoch()
+            print(f'Train avg. loss:\t{avg_loss_tr}')
+            if self.logging:
+                self.logger.info(f'Epoch: {i+1}')
+                self.logger.info(f'Train avg. loss:\t{avg_loss_tr}')
+            # Validate model
+            f1_val = self.evaluate('Validation')
+            # Check and update early stopping
+            if self.early_stopping:
+                if f1_val > (prev_f1 + 0.02):
+                    no_improve_cnt = 0
+                    prev_f1 = f1_val
+                else:
+                    no_improve_cnt += 1
+                # Early stop with no (significant) improvement for 3 epochs
+                if no_improve_cnt == 3:
+                    print(f'\n[Early stopping: no improvement for {no_improve_cnt} epochs]')
+                    break
+    
+    def evaluate_batch(self, batch):
+        '''
+        Evaluate a single batch and return loss, 
+        and predicted- and true labels of non-special tokens
+        '''
+        # Get data
+        input_ids = batch[0].to(self.device)
+        attention_mask = batch[1].to(self.device)
+        labels = batch[2].to(self.device)
+        # Get model output
+        with torch.no_grad():
+            output = self.model(input_ids = input_ids, 
+                                attention_mask = attention_mask,
+                                labels = labels)
+        loss = output['loss'].item()
+        # Get the label IDs predicted by the model
+        if 'predictions' in output.keys():
+            predictions = output['predictions']
+        else:
+            logits = output['logits']
+            predictions = torch.argmax(logits, dim=2)
+        # Get rid of special tokens (pad/sep/cls)
+        padding, separator, cls_token = self.get_added_tokens()
+        mask = ((input_ids != padding) & (input_ids != separator) & (input_ids != cls_token))
+        pred_list = torch.masked_select(predictions, mask).tolist()
+        label_list = torch.masked_select(labels, mask).tolist()
+        return loss, pred_list, label_list
+
+    def evaluate(self, datatype):
+        '''
+        Test the model (on validation or test set),
+        print metrics and return F1-score
+        '''
+        # Get either validation or test dataloader
+        if datatype == 'Validation':
+            data = self.dataloader.val_dataloader
+        else:
+            data = self.dataloader.test_dataloader
+        total_loss = 0
+        pred_list, y_list = [], []
+        # Set model to evaluation mode
+        self.model.eval()
+        # Evaluate each batch individually
+        for batch in data:
+            loss, preds, labels = self.evaluate_batch(batch)
+            total_loss += loss
+            # Save predictions and true labels for metrics
+            pred_list += preds
+            y_list += labels
+        # Calculate and print metrics
+        avg_loss = total_loss/len(self.dataloader.test_dataloader)
+        f1, precision, recall = self.get_metrics(y_list, pred_list)
+        print(f'{datatype} avg. loss:\t{avg_loss}')
+        print(f'{datatype} F1-score:\t{f1}')
+        print(f'{datatype} precision:\t{precision}')
+        print(f'{datatype} recall:\t{recall}')
+        if datatype == 'Test':
+            print(f'Classification report:\n{classification_report(y_list, pred_list)}')
+        if self.logging:
+            self.log_metrics(avg_loss, f1, precision, recall, datatype)
+        return f1
+        
